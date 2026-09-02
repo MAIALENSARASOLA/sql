83b40e4c23a6a420080d3e45e4922556a0fedd71
MAIALENSARASOLA <maialen.sarasola910@gmail.com>
2026-09-02 15:09:18 +0200

    Update 03_queries.sql

--- 03_queries.sql
+++ 03_queries.sql
@@ -8,11 +8,12 @@ from professors p
 join courses c on p.professor_id = c.professor_id
 join grades g on c.course_id = g.course_id
 group by p.first_name, p.last_name;
+
 -- mejor nota de cada alumno
 select s.first_name, s.last_name, max(g.grade) as max_nota
 from students s
 join grades g on s.student_id = g.student_id
-group by 1, 2;
+group by s.first_name, s.last_name;
 
 -- alumnos ordenados por nombre de curso
 select s.first_name, s.last_name, c.course_name
@@ -25,7 +26,7 @@ order by c.course_name;
 select c.course_name, avg(g.grade) as nota_media
 from courses c
 join grades g on c.course_id = g.course_id
-group by 1
+group by c.course_name
 order by nota_media asc;
 
 -- quien coincide mas con que profe
@@ -37,6 +38,6 @@ from students s
 join grades g on s.student_id = g.student_id
 join courses c on g.course_id = c.course_id
 join professors p on c.professor_id = p.professor_id
-group by 1, 2, 3, 4
+group by s.first_name, s.last_name, p.first_name, p.last_name
 order by total_comun desc
 limit 1;
