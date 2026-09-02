f6a460ccbcc144b459a3fc387545ed35e6141952
MAIALENSARASOLA <maialen.sarasola910@gmail.com>
2026-09-02 14:45:41 +0200

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,19 @@
+insert into students (first_name, last_name) values
+('Juan', 'Garcia'),
+('Maria', 'Lopez'),
+('Ane', 'Sanchez');
+
+insert into professors (first_name, last_name) values
+('Carlos', 'Martin'),
+('Laura', 'Perez');
+
+insert into courses (course_name, professor_id) values
+('Matematicas', 1),
+('Programacion', 1),
+('Bases de datos', 2);
+
+insert into grades (student_id, course_id, grade) values
+(1, 1, 8.50),
+(1, 2, 9.00),
+(2, 1, 7.00),
+(3, 3, 8.00);
